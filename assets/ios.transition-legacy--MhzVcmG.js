ef994ecd9f89dde47cf923615a53f052b3d480ef
angelqtypie <angeljustineballaso07@gmail.com>
2025-05-07 01:05:52 +0800

    Updates

--- assets/ios.transition-legacy--MhzVcmG.js
+++ assets/ios.transition-legacy--MhzVcmG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D8PNC9XO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-COH4XnIx.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
