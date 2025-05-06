973a6f63b597dcf53e2a7ffdb9ae768ba5c8015d
angelqtypie <angeljustineballaso07@gmail.com>
2025-05-07 01:40:48 +0800

    Updates

--- assets/md.transition-legacy-C-3ZmZei.js
+++ assets/md.transition-legacy-C-3ZmZei.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dq6P24E7.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BV6-UWKS.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
