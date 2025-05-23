83f40ab69c38ee3743c6ef46ccecfeba3250e67a
angelqtypie <angeljustineballaso07@gmail.com>
2025-05-23 23:53:41 +0800

    Updates

--- assets/ios.transition-legacy-BF5nX21b.js
+++ assets/ios.transition-legacy-BF5nX21b.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BV6-UWKS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Bf9op49V.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BL_giePZ.js
+++ assets/swipe-back-legacy-BL_giePZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BV6-UWKS.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Bf9op49V.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
