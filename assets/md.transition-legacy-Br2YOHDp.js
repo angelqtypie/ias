766502b0ee9201b77dceaf3b9a21aad686312b21
angelqtypie <angeljustineballaso07@gmail.com>
2025-05-24 22:59:59 +0800

    Updates

--- assets/md.transition-legacy-Br2YOHDp.js
+++ assets/md.transition-legacy-Br2YOHDp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bf9op49V.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Bs3ESmGk.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
