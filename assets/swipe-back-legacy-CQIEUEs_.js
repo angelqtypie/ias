15261e271492a549d9021a53ca099235989ab96f
angelqtypie <angeljustineballaso07@gmail.com>
2025-05-25 15:58:39 +0800

    Updates

--- assets/swipe-back-legacy-CQIEUEs_.js
+++ assets/swipe-back-legacy-CQIEUEs_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bs3ESmGk.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DzmGEeA0.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
